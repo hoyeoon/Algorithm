d732fd6c8490aae23dacd44d42b950f70c444b15
HOYEONCHOI <chy1995@ajou.ac.kr>
2021-08-21 23:56:12 +0900

    20210821

--- baekjoon/BOJ_15683.java
+++ baekjoon/BOJ_15683.java
@@ -10,6 +10,11 @@ import java.util.StringTokenizer;
 public class BOJ_15683 {
 	static int[] dx = {-1, 0, 1, 0};
 	static int[] dy = {0, 1, 0, -1};
+	static int[] rotateCnt = {0, 4, 2, 4, 4, 1};	// 회전 수 (1, 2, 3, 4, 5) idx를 1부터 사용하기 위해 0 추가
+	static int[] startIdx = {0, 0, 4, 6, 10, 14, 15};
+	// 1~5번 돌아야하는 방향(dx, dy의 idx)
+	static int[][] D = {{0}, {1}, {2}, {3}, {0, 2}, {1, 3}, {0, 1}, {1, 2}, {2, 3}, {3, 0}, 
+			{0, 1, 2}, {1, 2, 3}, {2, 3, 0}, {3, 0, 1}, {0, 1, 2, 3}};
 	
 	static List<int[]> cctv;
 	static int[][] map;
@@ -28,13 +33,9 @@ public class BOJ_15683 {
 			for(int j = 0; j < M; j++) {
 				int v = Integer.parseInt(st.nextToken());
 				map[i][j] = v;
-				if(v == 1 || v == 2 || v == 3 || v == 4 || v == 5)
-					cctv.add(new int[]{i, j});
+				if(v >= 1 && v <= 5) cctv.add(new int[]{i, j});
 			}
 		}
-		System.out.println(cctv);
-		System.out.println(Arrays.deepToString(map));
-		
 		recursive(0, map);
 		System.out.println(answer);
 	}
@@ -46,49 +47,52 @@ public class BOJ_15683 {
 		}
 		int x = cctv.get(cnt)[0];
 		int y = cctv.get(cnt)[1];
-		int v = map[x][y];
-		int n = 0;
-		switch(v) {
-			case 1: case 3: case 4:
-				n = 4;
-				break;
-			case 2:
-				n = 2;
-				break;
-			case 5:
-				n = 1;
-				break;
-		}
-		for(int i = 0; i < n; i++) {
-			switch(v) {
-			case 1:
+		int n = map[x][y];
+		int start = startIdx[n];
+		for(int i = start; i < start + rotateCnt[n]; i++) {
+			int[][] copyMap = copyMap(map);
+			
+			for(int j = 0; j < D[i].length; j++) {
+				int nx = dx[D[i][j]];
+				int ny = dy[D[i][j]];
 				
-				break;
-			case 2:
-				
-				break;
-			case 3:
-				
-				break;
-			case 4:
-				
-				break;
-			case 5:
-				
-				break;
+				int tempX = x;
+				int tempY = y;
+				while(true) {
+					tempX += nx;
+					tempY += ny;
+					
+					if(tempX < 0 || tempX >= N || tempY < 0 || tempY >= M || copyMap[tempX][tempY] == 6)
+						break;
+					
+					if(copyMap[tempX][tempY] != 0) {
+						continue;
+					}
+
+					copyMap[tempX][tempY] = 9;
+				}
+				recursive(cnt + 1, copyMap);
 			}
-			recursive(cnt + 1, map);
 		}
-		
 	}
 	
 	private static int count0(int map[][]) {
 		int cnt = 0;
-		for(int i = 0; i < map.length; i++) {
-			for(int j = 0; j < map[0].length; j++) {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
 				if(map[i][j] == 0) cnt++;
 			}
 		}
 		return cnt;
+	}
+	
+	private static int[][] copyMap(int map[][]){
+		int[][] copyMap = new int[N][M];
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				copyMap[i][j] = map[i][j];
+			}
+		}
+		return copyMap;
 	}
 }
