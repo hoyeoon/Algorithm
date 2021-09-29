91985c71a37672f047e9da571ac911809a83ee9a
HOYEONCHOI <chy1995@ajou.ac.kr>
2021-09-29 23:55:54 +0900

    20210929

--- baekjoon/BOJ_2146.java
+++ baekjoon/BOJ_2146.java
@@ -10,8 +10,8 @@ import java.util.StringTokenizer;
 public class BOJ_2146 {
 	static int N;
 	static int[][] map;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
+	static int[] dx = {1, 0, 0, -1};
+	static int[] dy = {0, -1, 1, 0};
 	static int land;
 	static int minBridge = Integer.MAX_VALUE;
 	
@@ -33,49 +33,59 @@ public class BOJ_2146 {
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
 				if(map[i][j] == 1) {
-					bfs(i, j);
+					markLand(i, j);
 				}
 			}
 		}
-		
-		System.out.println(Arrays.deepToString(map));
 		
 		// 두 대륙을 연결할 수 있는 가장 짧은 다리의 길이를 구하는 부분
+		for(int l = 2; l <= land; l++) {
+			connectLand(l);
+		}
+		
+		System.out.println(minBridge - 1);
+	}
+
+	private static void connectLand(int land) {
+		int[][] dist = new int[N][N]; 
+		Queue<int[]> q = new LinkedList<>();
+		
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
-				if(map[i][j] != 0) {
-					connectLand(i, j, 0, "");
+				if(map[i][j] == land) {
+					q.offer(new int[] {i, j});
+					dist[i][j] = 1;	// 현재 육지인 곳의 길이를 1로 초기화
 				}
 			}
 		}
 		
-		System.out.println(minBridge);
-	}
-
-	private static void connectLand(int i, int j, int cnt, String s) {
-		if(cnt >= minBridge) return;
-		
-		int curr = map[i][j];
-		
-		for(int d = 0; d < 4; d++) {
-			int nx = i + dx[d];
-			int ny = j + dy[d];
+		while(!q.isEmpty()) {
+			int[] loc = q.poll();
+			int x = loc[0];
+			int y = loc[1];
 			
-			if(nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] == curr) continue;
-			
-			if(map[nx][ny] != 0) {
-				minBridge = Math.min(minBridge, cnt);
-				System.out.println(s);
-				return;
+			for(int d = 0; d < 4; d++) {
+				int nx = x + dx[d];
+				int ny = y + dy[d];
+				
+				if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+				
+				// 다른 육지에 도달한 경우
+				if(map[nx][ny] != 0 && map[nx][ny] != land) {
+					minBridge = Math.min(minBridge, dist[x][y]);
+					return;
+				}
+				
+				// 이동할 수 있는 곳일 경우
+				if(map[nx][ny] == 0 && dist[nx][ny] == 0) {
+					q.offer(new int[] {nx, ny});
+					dist[nx][ny] = dist[x][y] + 1;
+				}	
 			}
-			
-			map[nx][ny] = curr;
-			connectLand(nx, ny, cnt + 1, nx +", " + ny+" ");
-			map[nx][ny] = 0;
 		}
 	}
 
-	private static void bfs(int i, int j) {
+	private static void markLand(int i, int j) {
 		Queue<int[]> q = new LinkedList<>();
 		q.offer(new int[] {i, j});
 		land++;
