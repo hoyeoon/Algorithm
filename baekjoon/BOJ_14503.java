335035c23fdd8e215ae77b29bf63e0b60d2f9d55
HOYEONCHOI <chy1995@ajou.ac.kr>
2021-10-09 23:54:27 +0900

    20211009

--- baekjoon/BOJ_14503.java
+++ baekjoon/BOJ_14503.java
@@ -7,11 +7,13 @@ import java.util.StringTokenizer;
 
 public class BOJ_14503 {
 	// 북, 동, 남, 서
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
 	static int N, M, r, c, d;
 	static int[][] map;
+	static int answer = 0;
 	static int cnt = 0;
+	static int flag = 1;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -33,12 +35,39 @@ public class BOJ_14503 {
 		}
 		System.out.println(Arrays.deepToString(map));
 		
-		map[r][c] = 1;
-		
-		
-		
 		while(true) {
+			int check = 0;
+			for(int d = 0; d < 4; d++) {
+				if(map[r + dr[d]][c + dc[d]] == 0) {
+					check = 1;
+					break;
+				}
+			}
+			// 네 방향 모두 청소가 이미 되어있거나 벽인 경우
+			if(check == 0) {
+				
+				
+				
+			}
 			
+			if(flag == 1) map[r][c] = 1;
+			d = (d + 3) % 4;
+			
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			
+			if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+			
+			if(map[nr][nc] == 0) {
+				r = nr;
+				c = nc;
+				flag = 1;
+				continue;
+			} else if(map[nr][nc] == 1) {
+				r = nr;
+				c = nc;
+				flag = 2;
+			}
 			
 			
 			
