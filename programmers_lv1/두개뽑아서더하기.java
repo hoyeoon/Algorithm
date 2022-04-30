0ecbc90fe733cd96ea9dbbb53dfd9c01bf476db6
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-04-30 23:52:16 +0900

    220430

--- programmers_lv1/두개뽑아서더하기.java
+++ programmers_lv1/두개뽑아서더하기.java
@@ -1,16 +1,24 @@
 package programmers_lv1;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class 두개뽑아서더하기 {
     public int[] solution(int[] numbers) {
-        List<Integer> list = new ArrayList<>();
-
+        Set<Integer> set = new TreeSet<>();
         int n = numbers.length;
 
-        // TreeSet 사용시 add 하면 자동으로 오름차순 정렬이 된다고 함
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().mapToInt(Integer::intValue).toArray();
+
+        /* 기존방식
+        List<Integer> list = new ArrayList<>();
+        int n = numbers.length;
+
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < n; j++){
                 list.add(numbers[i] + numbers[j]);
@@ -20,6 +28,6 @@ public class 두개뽑아서더하기 {
         List<Integer> duplicateRemoveList = new ArrayList<>(new HashSet<Integer>(list));
         System.out.println(duplicateRemoveList);
         duplicateRemoveList.sort(null); // null 대신 Comparator.naturalOrder() 가능
-        return duplicateRemoveList.stream().mapToInt(Integer::intValue).toArray();
+        return duplicateRemoveList.stream().mapToInt(Integer::intValue).toArray();*/
     }
 }
