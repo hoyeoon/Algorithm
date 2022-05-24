b64573abf311167d52ab13e150af7578b357c21b
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-05-24 23:47:39 +0900

    220524

--- programmers_lv2/가장큰정사각형찾기.java
+++ programmers_lv2/가장큰정사각형찾기.java
@@ -3,29 +3,24 @@ package programmers_lv2;
 public class 가장큰정사각형찾기 {
     public int solution(int[][] board)
     {
-        int answer = 1234;
         int N = board.length;
         int M = board[0].length;
-        int max_size = Integer.MIN_VALUE;
         int small_line = Math.min(N, M);
+        int max_size = Integer.MIN_VALUE;
 
-        outer1 : for(int n = 0; n < small_line; n++) {
-            for(int i = 0; i < N - n; i++){
-                outer2 : for(int j = 0; j < M - n; j++){
-                    // System.out.println(i + ", " +j + ", " + n);
+        if(small_line == 1) return 1;
 
-                    for(int x = 0; x < n + 1; x++){
-                        for(int y = 0; y < n + 1; y++){
-                            if(board[i + x][j + y] == 0) continue outer2;
-                            // System.out.println(i + x + ", " + (j + y));
-                        }
-                    }
-                    max_size = Math.max(max_size, (int)Math.pow(n + 1, 2));
-                    continue outer1;
+        for(int i = 1; i < N; i++){
+            for(int j = 1; j < M; j++){
+                int curr = board[i][j];
+
+                if(curr != 0 && board[i-1][j] >= curr && board[i][j-1] >= curr && board[i-1][j-1] >= curr){
+                    board[i][j] = Math.min(Math.min(board[i-1][j], board[i][j-1]), board[i-1][j-1]) + 1;
                 }
+                max_size = Math.max(max_size, board[i][j]);
             }
         }
 
-        return max_size;
+        return (int)Math.pow(max_size, 2);
     }
 }
